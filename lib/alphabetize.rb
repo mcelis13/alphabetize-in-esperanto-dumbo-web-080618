925c111cd0aaa79685c174326c1f7f39f34520d5
Madeline Celis <madelinebcelis@gmail.com>
2018-08-02 17:57:15 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+require 'i18n'
+I18n.enforce_available_locales = false
+
 def alphabetize(arr)
-  # code here
+
+  newArray = arr.sort_by do |e|
+    I18n.transliterate e
+  end
+
+  newArray
 end
